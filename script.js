6cbd35493666f94e82fa512b213ff1ab43a6c36f
munna-68 <mahamudmunna68@gmail.com>
2024-12-10 01:11:40 +0600

    Creating and adding functionality to the computerChoise function
    renaming the readme file with with all lowercase and working on the computerChoise function

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("Rock Paper Scissors");
+function getComputerChoise() {
+  let computerChoise;
+  let randomNumber = Math.floor(Math.random() * (4 - 1)) + 1;
+  switch(randomNumber) {
+    case 1:
+      computerChoise = 'rock';
+      break;
+   case 2:
+        computerChoise = 'paper';
+        break;
+    case 3:
+      computerChoise = 'scissor';
+      break;  
+  }
+  return computerChoise;
+}
+
+console.log(getComputerChoise())
